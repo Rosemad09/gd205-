6bc4cce87bf4764db0e7f7da98074d2732458520
Rosemad09 <charlievicioso17@gmail.com>
2023-10-05 16:36:15 -0400

    bascially
    yup

---
+++ Assets/script/alien.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alien : MonoBehaviour
+{
+Rigidbody rb;
+    public float forceMultiplier = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb = GetComponent<Rigidbody>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetKey(KeyCode.W)){
+            rb.AddForce(transform.forward * forceMultiplier);
+        }
+        if (Input.GetKey(KeyCode.S)){
+            rb.AddForce(-transform.forward * forceMultiplier);
+        }
+        if (Input.GetKey(KeyCode.A)){
+            rb.AddForce(-transform.right * forceMultiplier);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            rb.AddForce(transform.right * forceMultiplier);
+        }
+        if (Input.GetKey(KeyCode.LeftShift)){
+            rb.velocity *= 0.95f;
+        }
+         if (Input.GetKey(KeyCode.Space)){
+            rb.AddForce(transform.up * forceMultiplier);
+        }
+        if (Input.GetKey(KeyCode.Q))
+            rb.AddForce(-transform.up * forceMultiplier);
+        }
+         
+     }
+
