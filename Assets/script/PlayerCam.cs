306b00afbf3a16cc2d96b6c6ef4892e9a3a14a9c
Rosemad09 <charlievicioso17@gmail.com>
2023-11-29 11:10:54 -0500

    yea
    new upd

---
+++ Assets/script/PlayerCam.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float senseX;
+    public float senseY;
+    public Transform oriantation;
+    float xRotation;
+    float yRotation;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * senseX;
+
+
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senseY;
+
+
+        yRotation += mouseX;
+
+
+        xRotation -= mouseY;
+
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        oriantation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+    }
+
+}
